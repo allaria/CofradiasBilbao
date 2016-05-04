44a5dc0f81dbf5c1dfc16f3dadac8383d89728ad
allaria <allaria.tito@gmail.com>
2016-05-04 16:38:12 +0200

    comit

--- app/src/main/java/com/cofradias/android/model/callback/CofradiaService.java
+++ app/src/main/java/com/cofradias/android/model/callback/CofradiaService.java
@@ -14,6 +14,6 @@ public interface CofradiaService {
     //https://gist.githubusercontent.com/allaria/7d8b54b0bb755b7eb8abbffba25b6557/raw/9042a91e32103347c3954880a4c49a639de545b8/cofradiasv1.json
 
     //@GET("/allaria/7d8b54b0bb755b7eb8abbffba25b6557/raw/f6182d32ecf405453abb2c49d92a3cc803ee4921/cofradiasv1.json")
-    @GET("/allaria/1b9d87db649f8fd9667bc6b000e30242/raw/f9819d2b98dc94ae58ede43b418243f61d4251ad/cofradiasv2.json")
+    @GET("/allaria/1b9d87db649f8fd9667bc6b000e30242/raw/f349e65f5b143165b5ddaa1b25fd1bba2d4a9424/cofradiasv2.json")
     Call<List<Cofradia>> getAllCofradias();
 }
